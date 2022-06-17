eec0112d20252f2e2a0f31f48dbd5906ddc8f431
Perseu <josimar.17perseu@gmail.com>
2022-06-16 22:03:15 -0300

    feat:(post do login criado e funcionando)

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ import cors from 'cors';
 
 const app = Express();
 app.use(cors());
+app.use(Express.json());
+
+const usuario = {
+	username: '', 
+	avatar: "" 
+}
+
+
+const tweets = [];
+
+app.post('/sign-up', (req, res)=>{
+
+    usuario.username = req.body.username;
+    usuario.avatar = req.body.avatar;
+
+    res.send("ok");
+})
 
 
 
